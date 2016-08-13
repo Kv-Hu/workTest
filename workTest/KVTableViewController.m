0369ca9d3af4c217e0076dea9adcdc5fea7d0451
Kv-Hu <Kevin@komatoMacBook-Air.local>
2016-08-13 12:41:25 +0800

    C-Vim 顺序结构

--- workTest/KVTableViewController.m
+++ workTest/KVTableViewController.m
@@ -23,7 +23,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     DDTitleData *titleData = [[DDTitleData alloc] initWithTitle:self.title];
-    self.dataArray = @[titleData];
+    DDImageData *imageData = [[DDImageData alloc] init];
+    imageData.image = @"http://d.lanrentuku.com/down/png/1512/2015sdj/2015sdj_002.png";
+    self.dataArray = @[titleData,imageData];
     
 }
 
@@ -31,13 +33,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-//-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-//
-//    return 50;
-//
-//}
-
 
 
 
